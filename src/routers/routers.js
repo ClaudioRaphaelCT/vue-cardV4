0a6d1ac14fcf65ba9f30bc8b72d2cc8bf87c6d48
tsbcteixeira <167436439+tsbcteixeira@users.noreply.github.com>
2024-06-28 09:09:51 -0300

    tentativa de ajuste rotas

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -5,17 +5,21 @@ import Rhai from "@/views/Rhai.vue";
 import Rapha from "@/views/Rapha.vue";
 import Ambos from "@/views/Ambos.vue";
 import Buscar from "@/views/Buscar.vue";
+import ErrorNotFound from "@/views/ErrorNotFound.vue";
+
+const routes = [
+  { path: "/", name: "cartao", component: Cartao },
+  { path: "/cadastrar", name: "cadastrar", component: Cadastrar },
+  { path: "/rhai", name: "rhai", component: Rhai },
+  { path: "/rapha", name: "rapha", component: Rapha },
+  { path: "/ambos", name: "ambos", component: Ambos },
+  { path: "/buscar", name: "buscar", component: Buscar },
+  { path: "/:catchAll(.*)*", name: "not-found", component: ErrorNotFound },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    { path: "/", name: "cartao", component: Cartao },
-    { path: "/cadastrar", name: "cadastrar", component: Cadastrar },
-    { path: "/rhai", name: "rhai", component: Rhai, ignore: [".*"] },
-    { path: "/rapha", name: "rapha", component: Rapha },
-    { path: "/ambos", name: "ambos", component: Ambos },
-    { path: "/buscar", name: "buscar", component: Buscar },
-  ],
+  routes,
 });
 
 export default router;
---
+++ src/views/ErrorNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
+  >
+    <div>
+      <div style="font-size: 30vh">404</div>
+
+      <div class="text-h2" style="opacity: 0.4">Oops. Nothing here...</div>
+      <v-btn color="success" to="/">GO HOME!</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ErrorNotFound",
+});
+</script>
